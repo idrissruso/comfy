ddd00a4be43ae1d89e818faa2541ecf81bf1755f
Idrissa <idrisabkar2018@gmail.com>
2024-01-02 21:41:42 +0300

    Implemented routing with react-router-dom and set up AppLayout as the home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import AppLayout from './layout/AppLayout'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-  return AppLayout()
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<AppLayout />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,32 @@
+import { Link } from 'react-router-dom'
+
 function Navbar() {
   return (
-    <nav>
-      <h1 className=" bg-red-800">Navbar</h1>
+    <nav className="mt-4">
+      <Logo />
+      <NavItems />
     </nav>
   )
 }
-function NavItems() {
-  return <ul></ul>
+
+function Logo() {
+  return <img src="logo.png" alt="comfy_logo" className="w-[8rem]" />
 }
 
-function NavItem({ children }) {
-  return <li>{children}</li>
+function NavItems() {
+  return (
+    <ul>
+      <NavItem link="/" />
+    </ul>
+  )
+}
+
+function NavItem({ children, link }) {
+  return (
+    <li>
+      <Link to={link}>{children}</Link>
+    </li>
+  )
 }
 
 export default Navbar
