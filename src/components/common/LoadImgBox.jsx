044e7b5f534c45de0f5b28ef764ab10158fbab8f
Idrissa <idrisabkar2018@gmail.com>
2024-01-05 15:35:04 +0300

    Added search icon with hover effect to LoadImgBox component and set default props for width, height, and params

--- src/components/common/LoadImgBox.jsx
+++ src/components/common/LoadImgBox.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { BiSearch } from 'react-icons/bi'
 
 function LoadImgBox({ src, alt, params, width, height }) {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -12,22 +13,28 @@ function LoadImgBox({ src, alt, params, width, height }) {
       }
     }
   }, [isLoaded, src])
-  console.log(isLoaded)
+  console.log(width, height)
 
   return (
     <div
-      className={`relative  img-box rounded-md overflow-hidden w-[${width}] h-[${height}] ${
+      className={`group relative flex items-center justify-center img-box rounded-md overflow-hidden w-[23rem] h-[15rem] ${
         isLoaded ? 'no-after bg-transparent' : 'bg-secondary-400'
       } ${params}`}
     >
       {isLoaded && (
         <img
           loading="lazy"
-          className="rounded-md block w-full h-full object-cover object-center cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-2xl hover:brightness-50 back-dace-visibility"
+          className="rounded-md block w-full h-full object-cover object-center cursor-pointer group-hover:scale-105 transition-all duration-300 ease-in-out group-hover:shadow-2xl group-hover:brightness-50 back-face-visibility"
           src={src}
           alt={alt}
         />
       )}
+      <span
+        className="z-50 bg-secondary-400 flex justify-center items-center absolute rounded-full p-1.5 opacity-0
+         translate-y-[100%] transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-y-0"
+      >
+        <BiSearch className="text-2xl text-white back-face-visibility" />
+      </span>
     </div>
   )
 }
