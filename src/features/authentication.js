27224cce8261534684c8fdcd83bdb213358fd48f
Idrissa <idrisabkar2018@gmail.com>
2024-02-03 14:22:55 +0300

    Created useLogin custom hook for handling user login using React Query

--- src/features/authentication.js
+++ src/features/authentication.js
@@ -1,14 +1,6 @@
-import { useMutation } from 'react-query'
+import { useMutation } from '@tanstack/react-query'
 import { login } from '../Api/methods'
 
-export function useLogin({ email, password }) {
-  return useMutation({
-    mutationFn: () => login({ email, password }),
-    onSuccess: (data) => {
-      console.log('Login success:', data)
-    },
-    onError: (error) => {
-      console.error('Login error:', error)
-    },
-  })
+export function useLogin(name, password) {
+  return useMutation(() => login(name, password))
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import { LogInComponent } from '../components/auth/LogInComponent'
 import { Register } from '../components/auth/Register'
 import { Button } from '../components/auth/Button'
+import { useLogin } from '../features/authentication'
+import toast from 'react-hot-toast'
+import Loader from '../components/common/Loader'
 
 function Login() {
   const [login, setLogin] = useState(false)
+  const { mutate, isLoading, isError, isSuccess, error } = useLogin()
+
   function handleLogin(e) {
     e.preventDefault()
     const data = new FormData(e.target)
-    alert(data.get('name'))
+    const name = data.get('name')
+    const password = data.get('password')
+    const credentials = { name, password }
+    mutate(credentials)
   }
+
+  if (isLoading) return <Loader />
+  if (isError) {
+    toast.error(error)
+  }
+  if (isSuccess) {
+    toast.success('Logged in')
+  }
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-gradient-to-r from-secondary-300 via-secondary-200 to-secondary-100 transition-all duration-300">
       <div
